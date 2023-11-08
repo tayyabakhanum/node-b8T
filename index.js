3ec4e710a617acf1e8b8b73e9b9c5c8696774025
tayyabakhanum <132939199+tayyabakhanum@users.noreply.github.com>
2023-11-08 19:50:17 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express= require("express");
 
 const app= express();
-const PORT=3000;
+const PORT=4000;
 
 app.get("/", (req,res)=>{
     res.send(`welcome to the server from ${PORT} `)
-})
+});
 
+const employees=[
+     {name:"Tayyaba",
+    employee_code:2101},
+
+    {name:"Hasina",
+     employee_code:2102},
+
+     {name:"Naseera",
+     employee_code: 2103},
+
+     {name:"Bairose",
+      employee_code:2104}
+]
+ 
+app.get("/users",(req,res)=>{
+    res.send({
+        data:employees,
+        message:"success"
+    })
+})
 app.listen(PORT,()=>{
     console.log(`Server is running ${PORT}`)
 });
